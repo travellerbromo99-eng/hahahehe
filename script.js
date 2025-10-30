e396ee63e38ba07e38be4cd664f46224f91fbe0d
fadil tri wintaa <travellerbromo99@gmail.com>
2025-10-30 11:09:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const tombolsapa = document.getElementById('sapaButton");
+                                           tombolSapa.addEventListener('click',function(){
+                                             alert('Hallo! Terimakasih sudah jadi ultraman!');
+                                           });
