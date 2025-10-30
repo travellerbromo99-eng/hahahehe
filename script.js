1980ec121ba695b6eabc9e0e2962f2f5ff5d8cdc
fadil tri wintaa <travellerbromo99@gmail.com>
2025-10-30 11:28:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const tombolsapa = document.getElementById('sapaButton");
-                                           tombolSapa.addEventListener('click',function(){
-                                             alert('Hallo! Terimakasih sudah jadi ultraman!');
-                                           });
+// 1. Mengambil tombol dari HTML dan menyimpannya ke variabel 'tombolSapa'
+const tombolSapa = document.getElementById('sapaButton');
+                                            
+// 2. Menambahkan "pendengar kejadian" (event listener)
+tombolSapa.addEventListener('click', function() {
+                                             // 3. Aksi yang akan dijalankan saat tombol diklik
+                                             alert('Halo, Fadil! Terima kasih sudah jadi Ultraman!');
+                                           });
